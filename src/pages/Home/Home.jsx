0f8208095bdeb1f85f47e311b42d8e3b98c7feb3
b4kii <dami4nhalibart@gmail.com>
2023-02-28 21:47:53 +0100

    fix: try to fix images

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,16 +3,23 @@ import styles from "./Home.module.css";
 
 import { NavLink } from "react-router-dom";
 
-function MenuItem({ path, children, sideType, src }) {
+import FaqAccordionImage from "/src/assets/faq-accordion/design/desktop-preview.jpg";
+import IntroComponentImage from "/src/assets/intro-component/design/desktop-preview.jpg";
+import PricingComponentImage from "/src/assets/pricing-component/design/desktop-preview.jpg";
+import SocialProofImage from "/src/assets/social-proof-section/design/desktop-preview.jpg";
+import TipCalculatorImage from "/src/assets/tip-calculator/design/desktop-preview.jpg";
+
+function MenuItem({ path, children, src }) {
   return (
     <>
       <NavLink to={path} className={styles.menuLink}>
         <span className={styles.linkText}>{children}</span>
       </NavLink>
-      <div className={`${sideType} ${styles.sideWrapper}`}>
+      <div className={styles.sideWrapper}>
         <h1>PREVIEW</h1>
         <img
-          src={`src/assets/${src}/design/desktop-preview.jpg`}
+          // src={`src/assets/${src}/design/desktop-preview.jpg`}
+          src={src}
           alt={children}
           height="660"
           width="900"
@@ -26,41 +33,51 @@ export default function Home() {
   const menuRef = useRef();
 
   useEffect(() => {
-    Array.from(menuRef.current.getElementsByClassName(styles.menuLink)).forEach((item, index) => {
-      item.onmouseover = () => {
-        menuRef.current.dataset.activeIndex = index;
+    Array.from(menuRef.current.getElementsByClassName(styles.menuLink)).forEach(
+      (item, index) => {
+        item.onmouseover = () => {
+          menuRef.current.dataset.activeIndex = index;
+        };
       }
-    })
+    );
   }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles.menu} ref={menuRef} id={styles.menu}>
         <div className={styles.menuItems}>
-          <MenuItem path="/faq-accordion" sideType={styles.red} src="faq-accordion">
+          <MenuItem
+            path="/faq-accordion"
+            // src="faq-accordion"
+            src={FaqAccordionImage}
+          >
             Faq Accordion
           </MenuItem>
-          <MenuItem path="/intro-component" sideType={styles.green} src="intro-component">
+          <MenuItem
+            path="/intro-component"
+            // src="intro-component"
+            src={IntroComponentImage}
+          >
             Intro Component
           </MenuItem>
           <MenuItem
             path="/pricing-component"
-            sideType={styles.blue}
-            src="pricing-component"
+            // src="pricing-component"
+            src={PricingComponentImage}
           >
             Pricing Component
           </MenuItem>
           <MenuItem
             path="/social-proof-section"
-            sideType={styles.purple}
-            src="social-proof-section"
+            // src="social-proof-section"
+            src={SocialProofImage}
           >
             Social Proof Section
           </MenuItem>
           <MenuItem
             path="/tip-calculator"
-            sideType={styles.yellow}
-            src="tip-calculator"
+            // src="tip-calculator"
+            src={TipCalculatorImage}
           >
             Tip Calculator
           </MenuItem>
